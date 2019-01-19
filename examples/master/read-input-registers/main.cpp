59be9c846032454d702ac0a095dfcd1ac376a684
epsilonrt <epsilonrt@gmail.com>
2019-01-19 18:05:15 +0100

    Adds read-input-registers example

---
+++ examples/master/read-input-registers/main.cpp
@@ -0,0 +1,43 @@
+// libmodbuspp template
+// This example code is in the public domain.
+#include <iostream>
+#include <string>
+#include <modbuspp.h>
+
+using namespace std;
+using namespace Modbus;
+
+// -----------------------------------------------------------------------------
+int main (int argc, char **argv) {
+  string port ("/dev/ttyUSB1");
+
+  Master mb (Rtu, port , "38400E1"); // new master on RTU
+  // if you have to handle the DE signal of the line driver with RTS,
+  // you should uncomment the lines below...
+  // mb.rtu().setRts(RtsDown);
+  // mb.rtu().setSerialMode(Rs485);
+
+  if (mb.open ()) { // open a connection
+    // success, do what you want here
+    uint16_t values[2];
+
+    mb.setSlave (33); // to the slave at address 33
+    if (mb.readInputRegisters (1, values, 2) == 2) {
+
+      cout << "R0=" << values[0] << endl;
+      cout << "R1=" << values[1] << endl;
+    }
+    else {
+      cerr << "Unable to read input registers !"  << endl;
+      exit (EXIT_FAILURE);
+    }
+    mb.close();
+  }
+  else {
+    cerr << "Unable to open MODBUS connection to " << port << endl;
+    exit (EXIT_FAILURE);
+  }
+
+  return 0;
+}
+/* ========================================================================== */
---
+++ examples/master/read-input-registers/read-input-registers.mk
@@ -0,0 +1,110 @@
+##
+## Auto Generated makefile by CodeLite IDE
+## any manual changes will be erased      
+##
+## Debug
+ProjectName            :=read-input-registers
+ConfigurationName      :=Debug
+WorkspacePath          :=/home/pascal/src/libmodbuspp
+ProjectPath            :=/home/pascal/src/libmodbuspp/examples/master/read-input-registers
+IntermediateDirectory  :=./Debug
+OutDir                 := $(IntermediateDirectory)
+CurrentFileName        :=
+CurrentFilePath        :=
+CurrentFileFullPath    :=
+User                   :=epsilonrt
+Date                   :=19/01/19
+CodeLitePath           :=/home/pascal/.codelite
+LinkerName             :=/usr/bin/g++
+SharedObjectLinkerName :=/usr/bin/g++ -shared -fPIC
+ObjectSuffix           :=.o
+DependSuffix           :=.o.d
+PreprocessSuffix       :=.i
+DebugSwitch            :=-g 
+IncludeSwitch          :=-I
+LibrarySwitch          :=-l
+OutputSwitch           :=-o 
+LibraryPathSwitch      :=-L
+PreprocessorSwitch     :=-D
+SourceSwitch           :=-c 
+OutputFile             :=$(IntermediateDirectory)/$(ProjectName)
+Preprocessors          :=
+ObjectSwitch           :=-o 
+ArchiveOutputSwitch    := 
+PreprocessOnlySwitch   :=-E
+ObjectsFileList        :="read-input-registers.txt"
+PCHCompileFlags        :=
+MakeDirCommand         :=mkdir -p
+LinkOptions            := $(shell pkg-config --libs libmodbuspp) 
+IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
+IncludePCH             := 
+RcIncludePath          := 
+Libs                   := 
+ArLibs                 :=  
+LibPath                := $(LibraryPathSwitch). 
+
+##
+## Common variables
+## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
+##
+AR       := /usr/bin/ar rcu
+CXX      := /usr/bin/g++
+CC       := /usr/bin/gcc
+CXXFLAGS := -std=c++14 $(shell pkg-config --cflags libmodbuspp) -g -O0 -Wall $(Preprocessors)
+CFLAGS   := -std=c99 $(shell pkg-config --cflags libmodbuspp) -g -O0 -Wall $(Preprocessors)
+ASFLAGS  := 
+AS       := /usr/bin/as
+
+
+##
+## User defined environment variables
+##
+CodeLiteDir:=/usr/share/codelite
+Objects0=$(IntermediateDirectory)/main.cpp$(ObjectSuffix) 
+
+
+
+Objects=$(Objects0) 
+
+##
+## Main Build Targets 
+##
+.PHONY: all clean PreBuild PrePreBuild PostBuild MakeIntermediateDirs
+all: $(OutputFile)
+
+$(OutputFile): $(IntermediateDirectory)/.d $(Objects) 
+	@$(MakeDirCommand) $(@D)
+	@echo "" > $(IntermediateDirectory)/.d
+	@echo $(Objects0)  > $(ObjectsFileList)
+	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
+
+MakeIntermediateDirs:
+	@test -d ./Debug || $(MakeDirCommand) ./Debug
+
+
+$(IntermediateDirectory)/.d:
+	@test -d ./Debug || $(MakeDirCommand) ./Debug
+
+PreBuild:
+
+
+##
+## Objects
+##
+$(IntermediateDirectory)/main.cpp$(ObjectSuffix): main.cpp $(IntermediateDirectory)/main.cpp$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/pascal/src/libmodbuspp/examples/master/read-input-registers/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/main.cpp$(DependSuffix): main.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/main.cpp$(DependSuffix) -MM main.cpp
+
+$(IntermediateDirectory)/main.cpp$(PreprocessSuffix): main.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.cpp$(PreprocessSuffix) main.cpp
+
+
+-include $(IntermediateDirectory)/*$(DependSuffix)
+##
+## Clean
+##
+clean:
+	$(RM) -r ./Debug/
+
+
